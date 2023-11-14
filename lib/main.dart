3de9669579ce3bb6cb4beaf948a939956d426cc6
Hoosite <malandin.danil@mail.ru>
2023-11-14 18:27:06 +0500

    pageDan

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class StartPage extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => DanPage()),
+              MaterialPageRoute(builder: (context) => SecondPage()),
             );
           },
         ),
---
+++ lib/pagedan.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DanPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Стартовая страница'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Назад'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
